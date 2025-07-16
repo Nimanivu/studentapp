ab30d8572630e704761ad3a35e8f36c19f242f88
Nimanivu <nimajoshy2004@gmail.com>
2025-07-16 07:14:05 +0530

    updated

--- src/component/Addstudent.jsx
+++ src/component/Addstudent.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Studentnav from './Studentnav'
+import axios from 'axios'
 
 const Addstudent = () => {
+
+  const [input,inputChange]=useState(
+    {
+name: "",
+roll_number: "",
+admission_number: "",
+college: ""
+}
+    
+  )
+  const inputhandler=(event)=>{
+    inputChange({...input,[event.target.name]:event.target.value})
+  }
+
+  const readvalues=()=>{
+    console.log(input)
+
+    axios.post("http://18.144.111.41/student_api.php",input).then(
+      (response)=>{
+        alert("Successfully added")
+      }
+    ).catch()
+
+  }
   return (
     <div>
       <Studentnav/>
@@ -12,32 +37,32 @@ const Addstudent = () => {
                         <div className="col col-12 col-sm-6 col-md-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
 
                           <label htmlFor="" className="form-label">Name</label>
-                          <input type="text" className="form-control" />
+                          <input type="text" className="form-control" name='name' value={input.name} onChange={inputhandler} />
 
                         </div>
                         <div className="col col-12 col-sm-6 col-md-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
 
                          <label htmlFor="" className="form-label">Roll no</label>
-                          <input type="text" className="form-control" />
+                          <input type="text" className="form-control" name='roll_number' value={input.roll_number} onChange={inputhandler}/>
 
                         </div>
                         <div className="col col-12 col-sm-6 col-md-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
 
 
                           <label htmlFor="" className="form-label">Admission no</label>
-                          <input type="text" className="form-control" />
+                          <input type="text" className="form-control" name='admission_number' value={input.admission_number} onChange={inputhandler}/>
 
                         </div>
                         <div className="col col-12 col-sm-6 col-md-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
 
 
                          <label htmlFor="" className="form-label">College</label>
-                          <input type="text" className="form-control" />
+                          <input type="text" className="form-control" name='college' value={input.college} onChange={inputhandler}/>
 
                         </div>
                         <div className="col col-12 col-sm-6 col-md-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
 
-                            <button className="btn btn-success">Submit</button>
+                            <button className="btn btn-success" onClick={readvalues}>Submit</button>
                         </div>
                     </div>
                 </div>
--- src/component/Viewallstudent.jsx
+++ src/component/Viewallstudent.jsx
@@ -1,11 +1,21 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Studentnav from './Studentnav'
+import axios from 'axios'
 
 const Viewallstudent = () => {
   const [studentData, changeStudentData] = useState(
-    [{ "id": "1", "name": "John Doe", "roll_number": "R123", "admission_number": "A456", "college": "ABC College" }, { "id": "2", "name": "Rahul R", "roll_number": "1203", "admission_number": "A67456", "college": "ABC College" }, { "id": "3", "name": "Manu K", "roll_number": "35", "admission_number": "9792", "college": "SREE BUDHA COLLEGE " }, { "id": "4", "name": "Navaneeth", "roll_number": "24", "admission_number": "8268", "college": "SBC" }, { "id": "5", "name": "Madhav", "roll_number": "34524", "admission_number": "C342356", "college": "SBCE" }, { "id": "6", "name": "nabeel", "roll_number": "23", "admission_number": "23cs", "college": "sbce" }, { "id": "7", "name": "Vineeth Sreenivasan", "roll_number": "78", "admission_number": "R5626", "college": "Nirmala College" }, { "id": "8", "name": "Mehthab N M", "roll_number": "35", "admission_number": "N869", "college": "Nirmala College of Arts & Science" }, { "id": "9", "name": "Aneez", "roll_number": "2", "admission_number": "8268", "college": "Nirmala College of Arts & Science" }, { "id": "10", "name": "Devadath Pb", "roll_number": "24", "admission_number": "4568", "college": "Nirmala Collage of Arts & Science" }, { "id": "11", "name": "Aromal A S", "roll_number": "18", "admission_number": "12356", "college": "Nirmala Collage of Arts & Science" }] 
+    { "status": "success", "data": [] }
 
   )
+  const fetchdata = () => {
+    axios.get("http://18.144.111.41/view_all_students.php").then(
+      (response) => {
+        changeStudentData(response.data)
+      }
+    ).catch()
+
+  }
+  useEffect(()=>{fetchdata()},[])
   return (
     <div>
       <Studentnav />
@@ -13,6 +23,7 @@ const Viewallstudent = () => {
 
         <table class="table">
           <thead>
+            <th>View student</th>
             <tr>
               <th scope="col">Id</th>
               <th scope="col">Name</th>
@@ -23,18 +34,18 @@ const Viewallstudent = () => {
             </tr>
           </thead>
           <tbody>
-            {studentData.map(
-              (value,index)=>{
-                return(
-              <tr>
-              <th scope="row">{value.id}</th>
-              <td>{value.name}</td>
-              <td>{value.roll_number}</td>
-              <td>{value.admission_number}</td>
-              <td>{value.college}</td>
+            {studentData.data.map(
+              (value, index) => {
+                return (
+                  <tr>
+                    <th scope="row">{value.id}</th>
+                    <td>{value.name}</td>
+                    <td>{value.roll_number}</td>
+                    <td>{value.admission_number}</td>
+                    <td>{value.college}</td>
 
-            </tr>
-            
+                  </tr>
+
                 )
 
               }
